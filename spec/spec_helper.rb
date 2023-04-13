915bda2c5234d9b0054ef645841c7ebc6392dd9a
shinstar5 <peiyu.shinstar5@gapp.nthu.edu.tw>
2023-04-13 15:28:55 +0800

    spec helper create

---
+++ spec/spec_helper.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+ENV['RACK_ENV'] = 'test'
+
+require 'minitest/autorun'
+require 'minitest/rg'
+require 'yaml'
+
+require_relative 'test_load_all'
+
+def wipe_database
+  app.DB[:links].delete
+  app.DB[:platforms].delete
+end
+
+DATA = {} # rubocop:disable Style/MutableConstant
+DATA[:links] = YAML.safe_load File.read('app/db/seeds/link_seeds.yml')
+DATA[:platforms] = YAML.safe_load File.read('app/db/seeds/platform_seeds.yml')
