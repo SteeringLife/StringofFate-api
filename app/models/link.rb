c6092de0df3f72f1c461fd8ced294db5d883b5df
aresr417 <aresr417@gmail.com>
2023-04-13 17:17:46 +0800

    update model

--- app/models/link.rb
+++ app/models/link.rb
@@ -19,7 +19,7 @@ module StringofFate
             attributes: {
               id:,
               nickname:,
-              url:,
+              url:
             }
           },
           included: {
