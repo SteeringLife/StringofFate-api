b4462df3f5d6e2ee301788b80b47f7ddd337bf4a
YKGio <aresr417@gmail.com>
2023-04-27 12:15:43 +0800

    create account model

---
+++ app/db/migrations/001_accounts_create.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'sequel'
+
+Sequel.migration do
+  change do
+    create_table(:accounts) do
+      primary_key :id
+
+      String :username, null: false, unique: true
+      String :email, null: false, unique: true
+      String :password_digest
+      String :email
+      String :realname
+      String :showname
+      DateTime :created_at
+      DateTime :updated_at
+    end
+  end
+end
---
+++ app/models/account.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+require 'sequel'
+require 'json'
+require_relative './password'
+
+module StringofFate
+  # Models a registered account
+  class Account < Sequel::Model
+    one_to_many :owned_links, class: :'StringofFate::Link', key: :owner_id
+    many_to_many :connections,
+                 class: :'StringofFate::Account'
+                 join_table: :connection,
+                 left_key: :addresser_id, right_key: :requester_id
+
+    plugin :association_dependencies,
+           owned_links: :destroy
+           connections: :nullify
+
+    plugin :whitelist_security
+    set_allowed_columns :username, :email, :password
+
+    plugin :timestamps, update_on_create: true
+
+    def links
+      owned_links
+    end
+
+    def password=(new_password)
+      self.password_digest = Password.digest(new_password)
+    end
+
+    def password?(try_password)
+      digest = StringofFate::Password.from_digest(password_digest)
+      digest.correct?(try_password)
+    end
+
+    def to_json(options = {})
+      JSON(
+        {
+          type: 'account',
+          id:,
+          username:,
+          email:,
+          realname:,
+          showname:
+        }, options
+      )
+    end
+  end
+end
