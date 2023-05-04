e30700031264958da7c14dc92d0fea5677c5c97b
shinstar5 <peiyu.shinstar5@gapp.nthu.edu.tw>
2023-05-04 17:07:43 +0800

    dev seed

--- app/db/seeds/20230426_create_all.rb
+++ app/db/seeds/20230426_create_all.rb
@@ -4,24 +4,15 @@ Sequel.seed(:development) do
   def run
     puts 'Seeding accounts, platforms, links'
     create_accounts
-    create_platforms
-    create_owned_links
-    add_connections
   end
 end
 
 require 'yaml'
 DIR = File.dirname(__FILE__)
-LINK_INFO = YAML.load_file("#{DIR}/link_seeds.yml")
+ACCOUNTS_INFO = YAML.load_file("#{DIR}/accounts_seed.yml")
 
-def create_links
-  link_info_each = LINK_INFO.each
-  account_cycle = StringofFate::Account.all.cycle
-  loop do
-    link_info = link_info_each.next
-    account = account_cycle.next
-    StringofFate::CreateLinkForOwner.call(
-      account_id: account.id, link_data: link_info
-    )
+def create_accounts
+  ACCOUNTS_INFO.each do |account_info|
+    StringofFate::Account.create(account_info)
   end
 end
