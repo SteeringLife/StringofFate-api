e33162031b796df4bdf0591212d2d63e02f3f682
YKGio <aresr417@gmail.com>
2023-06-12 13:58:49 +0800

    test pass fix

--- app/services/create_private_hashtag_to_card_for_owner.rb
+++ app/services/create_private_hashtag_to_card_for_owner.rb
@@ -6,7 +6,7 @@ module StringofFate
   class CreatePrivateHashtagToCardForOwner
     class ForbiddenError < StandardError
       def message
-        'You are not allowed to create this tag'
+        'You are not allowed to create this private_hashtag'
       end
     end
 
@@ -19,17 +19,17 @@ module StringofFate
     def self.call(owner_id:, card_id:, private_hashtag_data:)
       owner = Account.find(id: owner_id)
       card = Card.find(id: card_id)
-      private_hashtag = PrivateHashtag.new(private_hashtag_data)
 
       policy = PrivateHashtagPolicy.new(owner, card, private_hashtag_data)
 
       raise ForbiddenError unless policy.can_create?
-      add_tag(owner, card, private_hashtag)
+      add_private_hashtag(owner, card, private_hashtag_data)
     end
 
-    def self.add_tag(owner, card, tag)
-      tag.card = card
-      owner.add_private_hashtag(tag)
+    def self.add_private_hashtag(owner, card, private_hashtag_data)
+      private_hashtag = PrivateHashtag.new(private_hashtag_data)
+      private_hashtag.card = card
+      owner.add_private_hashtag(private_hashtag)
     rescue Sequel::MassAssignmentRestriction
       raise IllegalRequestError
     end
